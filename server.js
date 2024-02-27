ff2bde50b5191f6a32a36296ccaaa536e2806cce
DifficultIV <kitpisan@gmail.com>
2024-02-28 02:33:55 +0700

    add sortQuery,recorddb,count,pages,mqtt

--- server.js
+++ server.js
@@ -4,92 +4,179 @@ import mqtt from "mqtt"
 import express from "express"
 import cors from "cors"
 const app = express()
-const dbFiles = fs.readFileSync('./db.json', 'utf-8')
-const dbFiles2 = fs.readFileSync('./dbnew.json', 'utf-8')
-const corsOption = {
-  origin: '*'
-}
-const latitude = [100, 200, 300, 400]
-const longitude = [200, 300, 400, 500]
-const db = JSON.parse(dbFiles)
-let db2 = JSON.parse(dbFiles2)
-console.log(db)
-
-for (let i = 0; i < latitude.length; i++) {
-  if (db2.length <= i) {
-    break;
-  }
-  if (db2[i].location.latitude <= latitude[i] && db2[i].location.longitude <= longitude[i]) {
-    db[i].in = db2[i].data.enter
-    db[i].out = db2[i].data.exit
-    db[i].current = db2[i].data.current
-  }
-}
-
-console.log(db)
-// app.get('/currentdb', cors(corsOption), (req, res) => {
-//   res.json(db)
-// })
+// const dbFiles = fs.readFileSync('./db.json', 'utf-8')
+// const dbFiles2 = fs.readFileSync('./dbnew.json', 'utf-8')
 const token = process.env.INFLUXDB_TOKEN
 const url = process.env.INFLUXDB_URL
 const org = process.env.INFLUXDB_ORG
 const bucket = process.env.INFLUXDB_BUCKET
+const influxdb = new InfluxDB({ url, token })
+const corsOption = {
+  origin: '*'
+}
+let count = 0
+const location = [[0, 100, 0, 100], [101, 200, 101, 200], [201, 300, 201, 300]]
+const stationdb = ["Station 1", "Station 2", "Station 3"]
+let db = []
+let allstationdb = [
+  {
+    busid: "",
+    station: "Station 1",
+    date: "",
+    time: "",
+    in: 0,
+    out: 1,
+    current: 2
+  },
+  {
+    busid: "",
+    station: "Station 2",
+    date: "",
+    time: "",
+    in: 0,
+    out: 1,
+    current: 2
+  },
+  {
+    busid: "",
+    station: "Station 3",
+    date: "",
+    time: "",
+    in: 0,
+    out: 1,
+    current: 2
+  },
+]
+
+const client = mqtt.connect("mqtts://11ba1af485354ff08eec334f40b97712.s1.eu.hivemq.cloud", {
+  username: '261492',
+  password: 'Proj_492'
+})
+
+client.on("connect", () => {
+  client.subscribe("test")
+});
+
+client.on("message", (topic, message) => {
+  const messageparse = JSON.parse(message.toString())
+  console.log(messageparse)
+  const writeApi = influxdb.getWriteApi(org, bucket)
+  writeApi.useDefaultTags({ Line: '1' })
+  for (let i = 0; i < location.length; i++) {
+    if ((messageparse.location.latitude >= location[i][0] && messageparse.location.latitude <= location[i][1]) && (messageparse.location.longitude >= location[i][2] && messageparse.location.longitude <= location[i][3])) {
+      const time = new Date(messageparse.time).toLocaleString('en-GB', { hourCycle: "h24" })
+      const time_split = time.split(", ")
+      db = [{
+        busid: messageparse.id,
+        station: stationdb[i],
+        date: time_split[0],
+        time: time_split[1],
+        in: messageparse.data.enter,
+        out: messageparse.data.exit,
+        current: messageparse.data.current
+      }]
+      const sortIndex = allstationdb.findIndex(obj => (obj.station == stationdb[i]))
+      if (sortIndex != -1) {
+        allstationdb[sortIndex].busid = db[0].busid
+        allstationdb[sortIndex].station = db[0].station
+        allstationdb[sortIndex].date = db[0].date
+        allstationdb[sortIndex].time = db[0].time
+        allstationdb[sortIndex].in = db[0].in
+        allstationdb[sortIndex].out = db[0].out
+        allstationdb[sortIndex].current = db[0].current
+      } else {
+        allstationdb.push(db)
+      }
+      const point1 = new Point(`Bus`)
+        .tag('busid', db[0].busid)
+        .tag('datestamp', time_split[0])
+        .tag('timestamp', time_split[1])
+        .tag('Station', db[0].station)
+        .intField('in', db[0].in)
+      console.log(` ${point1}`)
+      const point2 = new Point(`Bus`)
+        .tag('busid', db[0].busid)
+        .tag('datestamp', time_split[0])
+        .tag('timestamp', time_split[1])
+        .tag('Station', db[0].station)
+        .intField('out', db[0].out)
+      console.log(` ${point2}`)
+      const point3 = new Point(`Bus`)
+        .tag('busid', db[0].busid)
+        .tag('datestamp', time_split[0])
+        .tag('timestamp', time_split[1])
+        .tag('Station', db[0].station)
+        .intField('current', db[0].current)
+      console.log(` ${point3}`)
+      writeApi.writePoint(point1)
+      writeApi.writePoint(point2)
+      writeApi.writePoint(point3)
+      writeApi.close().then(() => {
+        console.log('WRITE FINISHED')
+      })
+      break
+    }
+
+
+  }
+});
+
+
+app.get('/currentdb', cors(corsOption), (req, res) => {
+  res.json(db)
+  console.log(db)
+})
+
 let querydb = []
 let sortdb = []
 
-const influxdb = new InfluxDB({ url, token })
-const writeApi = influxdb.getWriteApi(org, bucket)
-
-// writeApi.useDefaultTags({ Line: '1' })
-// let i = 0
-// while(i+1 <= db.length){
-//   console.log(db[i].station)
-// const point1 = new Point(`Bus ${db[i].busid}`)
-//   .tag('Station', db[i].station)
-//   .intField('in', db[i].in)
-// console.log(` ${point1}`)
-// const point2 = new Point(`Bus ${db[i].busid}`)
-//   .tag('Station', db[i].station)
-//   .intField('out', db[i].out)
-// console.log(` ${point2}`)
-// const point3 = new Point(`Bus ${db[i].busid}`)
-//   .tag('Station', db[i].station)
-//   .intField('current', db[i].current)
-//   console.log(` ${point3}`)
-// writeApi.writePoint(point1)
-// writeApi.writePoint(point2)
-// writeApi.writePoint(point3)
-// i+= 1
-// }
 
 const queryApi = new InfluxDB({ url, token }).getQueryApi(org)
-
 let fluxQuery =
   `from(bucket: "${bucket}")
     |> range(start: 0)
-    |> filter(fn: (r) => r._measurement == "Bus 1")`
+    |> group()
+    |> filter(fn: (r) => r._measurement == "Bus")
+    |> limit(n: 20,offset: 0)
+    `
 
+let fluxCountQuery =
+  `from(bucket: "${bucket}")
+    |> range(start: 0)
+    |> filter(fn: (r) => r._measurement == "Bus")
+    |> group(columns: ["_field"])
+    |> count()
+      `
+
+const countQuery = async () => {
+  count = 0
+  for await (const { values, tableMeta } of queryApi.iterateRows(fluxCountQuery)) {
+    const o = tableMeta.toObject(values)
+    // console.log(
+    //   `${o.datestamp} ${o.timestamp} ${o._measurement} ${o.busid} (${o.Station}): ${o._field}=${o._value}`
+    // )
+    count += o._value
+    console.log(count)
+  }
+}
 
 const myQuery = async () => {
   for await (const { values, tableMeta } of queryApi.iterateRows(fluxQuery)) {
     const o = tableMeta.toObject(values)
     // console.log(
-    //   `${o._time} ${o._measurement} (${o.Station}): ${o._field}=${o._value}`
+    //   `${o.datestamp} ${o.timestamp} ${o._measurement} ${o.busid} (${o.Station}): ${o._field}=${o._value}`
     // )
     querydb.push(o)
   }
 }
 
-writeApi.close().then(() => {
-  console.log('WRITE FINISHED')
-})
 
-await myQuery()
-
-for (const data of querydb) {
-  const time = new Date(data._time).toLocaleString('en-GB', { hourCycle: "h24" })
-  const time_split = time.split(", ")
-  const sortindex = sortdb.findIndex(obj => obj.date == time_split[0] && obj.time == time_split[1])
+// await myQuery()
+// await countQuery()
+// console.log(count)
+const sortQuery = () =>{
+  for (const data of querydb) {
+  const sortindex = sortdb.findIndex(obj => obj.date == data.datestamp && obj.time == data.timestamp)
   if (sortindex != -1) {
     if (data._field == "in") {
       sortdb[sortindex].in = data._value
@@ -103,8 +190,8 @@ for (const data of querydb) {
       sortdb.push({
         busid: data._measurement,
         station: data.Station,
-        date: time_split[0],
-        time: time_split[1],
+        date: data.datestamp,
+        time: data.timestamp,
         in: data._value,
         out: 0,
         current: 0
@@ -113,8 +200,8 @@ for (const data of querydb) {
       sortdb.push({
         busid: data._measurement,
         station: data.Station,
-        date: time_split[0],
-        time: time_split[1],
+        date: data.datestamp,
+        time: data.timestamp,
         in: 0,
         out: data._value,
         current: 0
@@ -123,8 +210,8 @@ for (const data of querydb) {
       sortdb.push({
         busid: data._measurement,
         station: data.Station,
-        date: time_split[0],
-        time: time_split[1],
+        date: data.datestamp,
+        time: data.timestamp,
         in: 0,
         out: 0,
         current: data._value
@@ -134,10 +221,39 @@ for (const data of querydb) {
   }
 
 }
-console.log(sortdb)
-// app.get('/db',cors(corsOption),(req,res)=>{
-//   res.json(querydb)
-// })
+}
 
-// app.listen(3000, () =>
-//     console.log('Start server at port 3000.'))
+
+// console.log(sortdb)
+app.get('/recorddb', cors(corsOption), async (req, res) => {
+  let page = req.query.pages
+  if (page == undefined) {
+    page = 0
+  }
+  console.log(page)
+  fluxQuery =
+    `from(bucket: "${bucket}")
+    |> range(start: 0)
+    |> group()
+    |> filter(fn: (r) => r._measurement == "Bus")
+    |> limit(n: 20,offset: ${page * 20})
+    `
+  await myQuery()
+  sortQuery()
+  res.json(sortdb)
+})
+
+app.get('/db', cors(corsOption), async (req, res) => {
+  res.json(allstationdb)
+})
+
+app.get('/count', cors(corsOption), async (req, res) => {
+  await countQuery()
+  let i = Math.ceil(count / 20)
+  res.json({
+    counts: count,
+    page: i
+  })
+})
+app.listen(3000, () =>
+  console.log('Start server at port 3000.'))
